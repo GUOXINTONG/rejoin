19acfc06b9e99fcfaeb40a9ea15f8f32eb2327f6
terryguo <terryguo0616@gmail.com>
2021-03-02 10:59:24 -0500

    local1

--- queries2db.py
+++ queries2db.py
@@ -6,7 +6,7 @@ import os
 
 def backup_queries():
 
-    cmd = "pg_dump -h localhost -p 5432 -U postgres -W  --table='queries' " \ 
+    cmd = "pg_dump -h localhost -p 5432 -U terryguo -W  --table='queries' " \
           "--data-only --column-inserts tpch > queries.sql" # change to tpch & imdbload and change to your user name
     try:
         os.system(cmd)
